daa8144f02458d146b7ed2c03a699152504c3870
Josiah Rooney <JosiahRooney@users.noreply.github.com>
2021-02-11 12:42:33 -0800

    Change unit types

--- src/types/Unit.type.ts
+++ src/types/Unit.type.ts
@@ -1,7 +1,13 @@
+export enum EUnit {
+  soldier = "soldier",
+  hunter = "hunter",
+}
+
 export type TUnit = {
   cost: number
   amount: number
   id: string
   label: string
   power: number
+  amount_free: number
 }
